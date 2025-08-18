e1674e3d576aab53fc184ed9792cc496351dc8a4
mauricio <mki.araujo@gmail.com>
2025-08-18 19:55:12 -0300

    New project files

---
+++ src/main/java/com/algaworks/algaposts/textprocessorservice/TextprocessorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaposts.textprocessorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextprocessorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextprocessorserviceApplication.class, args);
+	}
+
+}
